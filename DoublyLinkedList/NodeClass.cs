4143aef697251178b20fa7c7c50a3acc4e49741b
Carlos Gutierrez <carlosgutz25@gmail.com>
2017-10-08 16:45:29 -0700

    Adding Map

---
+++ DoublyLinkedList/NodeClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DoublyLinkedList
+{
+    public class NodeClass
+    {
+        public NodeClass(string name=null)
+        {
+            if (string.IsNullOrEmpty(name) || name == "")
+            {
+                
+            }
+        }
+        public void Add_Pointer(List<int> pointer)
+        {
+            
+        }
+    }
+}
